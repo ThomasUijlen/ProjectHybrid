b51ad4ad4722105a843e417e7203e89e94952b1d
ThomasUijlen <thomasuijlen@hotmail.com>
2023-01-17 22:09:33 +0100

    Projection scene

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -38,8 +38,12 @@ func updateEmotionCircle():
 func _process(delta):
 	var image : Image = $SubViewport.get_texture().get_image()
 	var texture : Texture = ImageTexture.create_from_image(image)
-	$Node3D/Decal.texture_albedo = texture
-	$Node3D/Decal.texture_emission = texture
+	
+	if has_node("Node3D"):
+		$Node3D/Decal.texture_albedo = texture
+		$Node3D/Decal.texture_emission = texture
+#	else:
+#		$Image.texture = texture
 #		$EmotionCircle.material.set("shader_param/"+emotion+"Amount", score)
 
 func spawnLabel():
@@ -54,13 +58,41 @@ func spawnLabel():
 			strongestScore = score
 			strongestEmotion = emotion
 	
-	label.setColor(getColor(strongestEmotion))
+	var color = getColor(strongestEmotion)
+	label.setColor(color)
 	
 	$SubViewport.add_child(label)
-	label.position = Vector2(randf_range(0,1000),randf_range(0,1000))
+	label.position = (findPixel(color)/pixels)*1000
+	label.position += Vector2.ONE*((0.5/25)*1000)
 	var direction = label.position.direction_to(Vector2(500,500))
 	label.look_at(label.position - direction)
 
+func findPixel(c : Color):
+	var validCoords = []
+	
+	for x in range(2,pixels-2):
+		for y in range(2,pixels-2):
+			var coord = Vector2(x,y)
+			if image.get_pixel(coord.x, coord.y) == c: continue
+			
+			var validCount = 0
+			validCount += isValid(coord + Vector2.LEFT, c)
+			validCount += isValid(coord + Vector2.RIGHT, c)
+			validCount += isValid(coord + Vector2.UP, c)
+			validCount += isValid(coord + Vector2.DOWN, c)
+			
+			if validCount > 0: validCoords.append(coord)
+	
+	if validCoords.size() > 0:
+		validCoords.shuffle()
+		return validCoords[0]
+	else:
+		return Vector2(randf_range(0,pixels),randf_range(0,pixels)).floor()
+
+func isValid(pixel : Vector2, color : Color):
+	if pixel.x >= pixels || pixel.y >= pixels: return 0
+	if image.get_pixel(pixel.x, pixel.y) != color: return 0
+	return 1
 
 func setPixelColor(rawPosition : Vector2, color : Color):
 	rawPosition /= 1000.0
@@ -87,3 +119,11 @@ func getColor(emotion):
 		c = Color("8338EC")
 	
 	return c
+
+func _input(event):
+	if event.is_action_pressed("Up"):
+		$Image.scale.x = $Image.scale.x + 0.1
+		$Image.scale.y = $Image.scale.y + 0.1
+	if event.is_action_pressed("Down"):
+		$Image.scale.x = $Image.scale.x - 0.1
+		$Image.scale.y = $Image.scale.y - 0.1
